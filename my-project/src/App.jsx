789985ae02834345b10ae75524f7bdbcb11a5143
wacho91 <criskol.71@gmail.com>
2024-08-12 10:49:42 -0500

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Nabvar from "./components/Navbar/Nabvar"
+
+
+const App = () => {
+  return (
+    <main>
+      <Nabvar />
+    </main>
+  )
+}
+
+export default App
---
+++ my-project/src/components/Navbar/Nabvar.jsx
@@ -0,0 +1,62 @@
+import Logo from "../../assets/logo.png"
+
+const MenuData = [
+  {
+    id: 1,
+    title: "Home",
+    url: "#",
+  },
+  {
+    id: 2,
+    title: "Youtube Blueprint Course",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "ChatGPT Course",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "Time Management Course",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "E-books",
+    url: "#",
+  },
+];
+
+const Nabvar = () => {
+  return (
+    <nav className="bg-black text-white py-3">
+      <div className="container flex justify-between">
+        {/*Logo Section*/}
+        <div>
+          <img src={Logo} alt="" className="max-w-[120px]" />
+        </div>
+        {/*Menu Section*/}
+        <div>
+          <ul className="flex items-center gap-4">
+            {MenuData.map((item) => {
+              return (
+                <li key={item.id}>
+                  <a href={item.url} className="uppercase text-xs" >
+                    {item.title}
+                  </a>
+                </li>
+              )
+            })}
+            <li>
+              <button className="border border-white px-3 py-1 rounded-full">Login</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nabvar
+
