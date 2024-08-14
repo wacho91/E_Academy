31116abc3c72c11d3ac7a4e5b1e88a4b535a2639
wacho91 <criskol.71@gmail.com>
2024-08-14 09:50:48 -0500

    Changes

--- my-project/src/components/Testimonials/TestimonialCard.jsx
+++ my-project/src/components/Testimonials/TestimonialCard.jsx
@@ -3,16 +3,18 @@
 const TestimonialCard = ({id, name, review, rating}) => {
   return (
     <>
-        <section className="p-12 bg-[#222222] rounded-3xl">
-            <p className="text-6xl font-serif rotate-180 inline-block">,,</p>
-            <p>{review}</p>    
-            <div className="flex items-center gap-4 mt-8">
-                <div className="h-10 w-10 flex justify-center items-center text-black bg-white rounded-full">
-                    {name.charAt(0).toUpperCase()}
-                </div>
-                <div>
-                    <p>{name}</p>
-                    <p>🌟🌟🌟🌟🌟</p>
+        <section className="p-12 bg-[#222222] rounded-3xl flex justify-center items-center">
+            <div>
+                <p className="text-6xl font-serif rotate-180 inline-block">,,</p>
+                <p>{review}</p>    
+                <div className="flex items-center gap-4 mt-8">
+                    <div className="h-10 w-10 flex justify-center items-center text-black bg-white rounded-full">
+                        {name.charAt(0).toUpperCase()}
+                    </div>
+                    <div>
+                        <p>{name}</p>
+                        <p>🌟🌟🌟🌟🌟</p>
+                    </div>
                 </div>
             </div>
         </section> 
